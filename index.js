05d0b3f243d6880f20e61a465c4fac947b524cb0
Luis Escobar <lae311@stern.nyu.edu>
2021-10-16 16:09:42 -0400

    commit tree c30f49ebf779b22d30f1700753c4bf45ee61b939

--- index.js
+++ index.js
@@ -11,6 +11,53 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+
+  const result = tutorials.map(title => {
+    // first it's iterating through the array
+    let splitWords = title.split(" ")
+    // split each title in word array
+    return capitalized(splitWords);    
+    // as it does that it sends it to the capitalized function
+  })
+
+  function capitalized(wordArray) {
+  // iterating through each word
+  // as it's doing that it's capilizing the first letter in each word and saves it into capital title
+  // let firstLetter = word[0]
+  let capitalTitle = wordArray.map(word => word.replace(word[0], word[0].toUpperCase()))
+  // joins the capital title array into sentences again
+  return capitalTitle. join ( ' ')
+  }
+
+// the last step is to return the joined result array which has the solution
+  return result;
 }
+
+
+// const titleCased = (tutortials) => {
+//   const lowerCase = tutorials.map(function(title) {
+//     return title.toLowerCase()
+//   })
+//   const wordSeperate = lowerCase.map(function(title) {
+//     return title.split(' ')
+//   })
+//   const letterSeperate = wordSeperate.map(function(word) {
+//     for (let i = 0; i < word.length; i++) {
+//       return word[i][0].toUpperCase() + word[i][i]
+//     }})
+//   return letterSeperate
+// }
+//   const letterSeperate = wordSeperate.map(function(word) {
+//     return word
+//   })
+//   con
+//   return letterSeperate;
+// }
+
+
+// const step2 = step1.map(function(word) {
+//   
+// return word[0].toUpperCase() + word.substring(1)
+// })
